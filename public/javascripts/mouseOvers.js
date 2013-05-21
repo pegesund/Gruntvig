74d6e5d0553cf8652a7e31e9410867563c57f353
petter <petter@c6960bf51.dhcp.as2116.net>
2013-05-21 21:11:39 +0200

    Open i new-tab links, docOut

--- public/javascripts/mouseOvers.js
+++ public/javascripts/mouseOvers.js
@@ -99,6 +99,11 @@ var addDocOutListener = function() {
         options["scrollToObject"] = $(this).attr("name");
         options["scrollToAdjust"] = $(this).position().top;
         var target = $(this).attr("name");
+        if (!target.indexOf("#") !== -1) {
+            var win=window.open(target, '_blank');
+            win.focus();
+            return;
+        }
         var link = docOutTodocInLink(target);
         var scrollToInNewWindow = link[0] + "_" + link[1] + "_" + link[2];
         options["scrollToInNewWindows"] = scrollToInNewWindow;
