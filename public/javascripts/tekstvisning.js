afee372dd54eac43990a748d19ae654ddefe3a8d
karsten <karsten@D05940>
2013-11-21 16:23:26 +0100

    Move blank()

--- public/javascripts/tekstvisning.js
+++ public/javascripts/tekstvisning.js
@@ -106,3 +106,12 @@ $(document).ready(function() {
     // addSimpleReader({}, "Pettertest", "1804_28_txr.xml");                
 });
 
+ 
+        // Visning af epitekst, biblDesc mv. i separat vindue, KK 2013-11-21
+function blank(target, loc, x, y) {
+    if ( !x) { x= "675"; }
+    if ( !y) { y= screen.availHeight-100; }
+    var w= window.open(loc,target,"width="+x+",height="+y+",resizable=yes,scrollbars=yes,status=no,menubar=no,titlebar=no,location=yes",true);
+    w.focus();
+    return false;
+    }
