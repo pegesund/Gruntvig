e901174de8728660752c6e7f5590db97595941c9
ksr <ksr@D01770>
2013-12-06 15:52:57 +0100

    Kolofon
    flyttet tilbage på sin gamle plads; afventer løsning

--- app/models/Chapter.java
+++ app/models/Chapter.java
@@ -152,19 +152,16 @@ public class Chapter extends GenericModel {
                     Node node = nodes.item(i);
                     // System.out.println("Chapter node: " + Helpers.nodeToString(node));
                     // System.out.println("---------------------------------------------------");
-                    String name = "- Afsnit - " + (i + 0);
-                    if (i == 0) name = "Kolofon";
+                    String name = "- afsnit mangler titel";
+                    if (i == nodes.getLength()-1) name = "[Kolofon]";
                     if (node.getAttributes().getNamedItem("name") != null) {
                         name = node.getAttributes().getNamedItem("name").getNodeValue();
                         System.out.println("Chapter id found: " + name);
-                    if (i == 0) name = "Kolofon";
                     }                    
-                    
                     if (node.getAttributes().getNamedItem("rend") != null) {
                         name = node.getAttributes().getNamedItem("rend").getNodeValue();
                         System.out.println("Chapter id found: " + name);
                     }                    
-                    
                     Chapter chapter = new Chapter(name, i, asset, nodeToString(node));
                     chapter.save();
                     // System.out.println("Chapter: " + i + nodeToString(node));
