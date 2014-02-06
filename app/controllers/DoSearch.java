b7b8913b930fe756753b86dba27975f8f034d2eb
karsten <karsten@D05940>
2014-02-06 17:38:08 +0100

    Avanceret søgning, søgeside

--- app/controllers/DoSearch.java
+++ app/controllers/DoSearch.java
@@ -132,10 +132,12 @@ public class DoSearch extends Application {
      * Ændret ifm. avanceret søg, KK 2014-02-05 */
     private static String createTeaser(String str, String lookforOrig, int len) {
         String lookfor = lookforOrig.toLowerCase().replace("*","\\p{L}*").replace("?","\\p{L}");
-        if( lookfor.contains("\"") && !lookfor.contains("~") )
+        if( lookfor.contains("\"") && lookfor.contains("~") )
+            lookfor = lookfor.replace(" ",".*").replaceAll("~[0-9.]*","").replace("\"","");
+        else if( lookfor.contains("\"") )
             lookfor = lookfor.replace("\"","");
         else
-            lookfor = lookfor.replace(" ","|").replaceAll("~[0-9.]*","").replace("\"","");
+            lookfor = lookfor.replace(" ","|");
         System.out.println("Look for: " + lookfor );
         int lookforStart;
         //Pattern findWordsPattern = Pattern.compile("(\\s" + lookfor + "|^" + lookfor +")" +"[ ,;!.]", Pattern.CASE_INSENSITIVE);
