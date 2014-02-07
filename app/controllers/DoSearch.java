48160faf75d8a42508fb722ddf3bf76723296b30
karsten <karsten@D05940>
2014-02-07 14:46:37 +0100

    Avanceret søgning, søgeside

--- app/controllers/DoSearch.java
+++ app/controllers/DoSearch.java
@@ -137,8 +137,8 @@ public class DoSearch extends Application {
             int n= lookfor.indexOf("~")+1;
             int m= n+1;
             while( m<lookfor.length() && lookfor.substring(n,m).matches("[0-9]+") ) {
-                m++;
                 System.out.println( n+","+m+","+lookfor.substring(n,m) );
+                m++;
             }
             prox= (m==n+1)?0:Integer.parseInt(lookfor.substring(n,m));
             lookfor = lookfor.replace(" ","|").replaceAll("~[0-9.]*","").replace("\"","");
