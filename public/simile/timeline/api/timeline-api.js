7148df81b20b036fe3938332ba82e96b6b4cd178
kimravn <ksr@D01770>
2014-03-13 15:36:25 +0100

    test local

--- public/simile/timeline/api/timeline-api.js
+++ public/simile/timeline/api/timeline-api.js
@@ -253,8 +253,8 @@
         window.SimileAjax_onLoad = loadMe;
         
         var url = useLocalResources ?
-            "http://127.0.0.1:9999/ajax/api/simile-ajax-api.js?bundle=false" :
-            "http://static.simile.mit.edu/ajax/api-2.2.0/simile-ajax-api.js";
+            "../public/simile/ajax/api/simile-ajax-api.js?bundle=true" :
+            "../public/simile//ajax/api-2.2.0/simile-ajax-api.js";
         if (typeof Timeline_ajax_url == "string") {
            url = Timeline_ajax_url;
         }
