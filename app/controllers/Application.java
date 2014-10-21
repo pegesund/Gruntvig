4a4afa0da0fa4fc00f73597f290dd79e53a65b0c
Kim <kim@kim.dk>
2014-10-21 10:07:57 +0200

    fix kronologisk visning

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -50,7 +50,7 @@ public class Application extends Controller {
           litra= sj.substring( sj.length()-1 );
           sj= sj.substring( 0, sj.length()-1 );
         }
-        res= String.format( "%4s%4s%2s%s", year, sj, litra, sjSub ); /*KK 2014-01-08 sjSub er en dårlig ide*/
+        res= String.format( "%4s%4s%s%2s", year, sj, litra, sjSub ); /*KK 2014-10-08 sjSub er en dårlig ide*/
         //System.out.println( ">"+res );
         return res;
     }
